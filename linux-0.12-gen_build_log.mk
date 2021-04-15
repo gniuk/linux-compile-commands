9e1dfdc1014b1d4eeb61165c56814e9245c1b02e
gniuk <isgniuk@gmail.com>
2021-04-15 14:22:27 +0800

    Tune env settings, LANGUAGE alone is enough

--- linux-0.12-gen_build_log.mk
+++ linux-0.12-gen_build_log.mk
@@ -1,5 +1,5 @@
 SUBDIRS = kernel/math kernel/blk_drv kernel/chr_drv kernel mm fs lib
 
 all:
-	for i in ${SUBDIRS}; do LANG=en_US.UTF-8 LANGUAGE=en make -C $$i V=1 -j1 --dry-run; done
-	LANG=en_US.UTF-8 LANGUAGE=en make V=1 -j1 --dry-run init/main.o
+	for i in ${SUBDIRS}; do LANGUAGE=en make -C $$i V=1 -j1 --dry-run; done
+	LANGUAGE=en make V=1 -j1 --dry-run init/main.o
--- linux-0.12-gen_build_log.sh
+++ linux-0.12-gen_build_log.sh
@@ -3,11 +3,10 @@
 _subdirs=(kernel/math kernel/blk_drv kernel/chr_drv kernel mm fs lib)
 
 for _dir in ${_subdirs[@]}; do
-  LANG=en_US.UTF-8 LANGUAGE=en make -C ${_dir} V=1 -j1 --dry-run |& tee -a build-log.txt
+  LANGUAGE=en make -C ${_dir} V=1 -j1 --dry-run |& tee -a build-log.txt
 done
-LANG=en_US.UTF-8 LANGUAGE=en make V=1 -j1 --dry-run init/main.o |& tee -a build-log.txt
+LANGUAGE=en make V=1 -j1 --dry-run init/main.o |& tee -a build-log.txt
 
 # compiledb needs the make[N] style of Entering and Leaving directory.
 # make[N] is automatically set when these commands are executed in Makefile target. See gen_build_log.mk
 sed -i 's|make|make[1]|g' build-log.txt
-
